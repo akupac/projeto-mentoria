21dbf385aa35775f5d8f257b831968075e7b6fee
Trampant <akupac@gmail.com>
2021-02-20 11:09:32 -0300

    primeiras linhas

---
+++ pages/mentoria.js
@@ -0,0 +1,20 @@
+const Mentoria = () => {
+
+const variavel = 'ola'
+const variavel-multinhas = `ola
+multinhas
+
+pode ser usado pra adicionar CSS`
+  return (
+
+    <>
+    <p>Olá, Mentoria</p>
+    <p>Tudo belezinha?</p>
+    {variavel}
+    <style jsx>{``}</style>
+    </>
+  )
+
+}
+
+export default Mentoria;
